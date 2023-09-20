16f9d769c959598f50f635babc2b2249750f86f1
Michal <michal513717@gmail.com>
2023-09-20 14:14:32 +0200

    Removed unused assets and added common routes

---
+++ apps/server/src/common/common.routes.config.ts
@@ -0,0 +1,53 @@
+import type { Application } from "express";
+
+
+export abstract class CommonRoutesConfig {
+
+  private routeName: string;
+  private version: string;
+  protected app: Application;
+
+  public static statusMessage = {
+    FAILED: "Failed",
+    SUCCESS: "Success",
+  };
+
+  protected static routeType = {
+    PUT: "PutRoutes",
+    GET: "GetRoutes",
+    POST: "PostRoutes",
+    PATCH: "PatchRoutes",
+    DELETE: "DeleteRoutes",
+    SOCKET: "SocketRoutes",
+    NOT_VALID: "NotValidRoutes",
+    NOTIFICATIONS: "NotificationsRoutes",
+    ASSETS: "Assets control",
+    AUTH: "Auth control",
+    GAMES_OPTIONS: "Games options",
+    QUIZZES: "Quizzes control",
+    SURVEYS: "Surveys control",
+    USERS: "Users control",
+  };
+
+  constructor(app: Application, routeName: string, version: string) {
+    this.routeName = routeName;
+    this.app = app;
+    this.version = version;
+
+    this.configureRoute();
+  }
+
+  public getName(): string {
+    return this.routeName;
+  }
+
+  public getVersion(): string {
+    return this.version;
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+
+  abstract configureRoute(): Application;
+}
